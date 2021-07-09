aec536fb2d4ec6a0bfea2e8a614095173aab809a
thomas buglioni <thomas.buglioni@gmail.com>
2021-07-09 23:10:54 +0200

    add nav bar

--- unlimited_studies/views.py
+++ unlimited_studies/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def home_screen_view(request):
     context = {}
-    context["some_string"] = "this is some string from view :) "
+    context["some_string"] = "Why learn ?? because ..."
 
     return render(request, "home.html", context)
 
