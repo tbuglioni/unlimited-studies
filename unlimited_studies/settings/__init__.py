e0d2c6c5e6cd93b969cb5c4742421b261d5614d1
thomas buglioni <thomas.buglioni@gmail.com>
2021-09-03 23:52:54 +0200

    update front account + home

--- unlimited_studies/settings/__init__.py
+++ unlimited_studies/settings/__init__.py
@@ -121,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = "fr-fr"
+LANGUAGE_CODE = "en-en"
 
 TIME_ZONE = "Europe/Paris"
 
